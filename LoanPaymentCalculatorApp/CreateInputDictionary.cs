76ad8b0b1dccbda4d48c4b60ac36383328008fb3
Russ Coleman <rucoleman@users.noreply.github.com>
2018-05-31 08:04:03 -0700

    Move most of the logic out of Main

---
+++ LoanPaymentCalculatorApp/CreateInputDictionary.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleAppAryFromUserInput
+{
+    class CreateInputDictionary
+    {
+        public class Pair
+        {
+            public string v1 { get; set; }
+            public string v2 { get; set; }
+
+
+            public Pair(string v1, string v2)
+            {
+                this.v1 = v1;
+                this.v2 = v2;
+            }
+        }
+        public static Dictionary<string, string> CreateDict()
+        {
+            const int MAXLINES = 10;
+            const int EXPECTEDLINECOUNT = 4;
+
+            List<Pair> input = new List<Pair>();
+
+            for (int i = 0; i < MAXLINES; i++)
+            {
+                string inputstr = Console.ReadLine();
+
+                if (inputstr is null || inputstr.Length == 0)
+                {
+                    break;
+                }
+                string[] inary = inputstr.Split(':');
+                input.Add(new Pair(inary[0].Trim().ToLower(), inary[1].Trim().ToLower()));
+            }
+            int linesgiven = input.Count;
+            if (linesgiven != EXPECTEDLINECOUNT)
+            {
+                Console.WriteLine(string.Format("Unexpected number of input lines: {0}, expecting {1}", linesgiven, EXPECTEDLINECOUNT));
+            }
+            Dictionary<string, string> dict = new Dictionary<string, string>();
+            foreach (Pair pair in input)
+            {
+                dict.Add(pair.v1, pair.v2);
+            }
+
+            return dict;
+        }
+    }
+}
---
+++ LoanPaymentCalculatorApp/LoanCalcInputFromDict.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using LoanCalc;
+
+namespace ConsoleAppAryFromUserInput
+{
+    class LoanCalcInputFromDict
+    {
+        private Dictionary<string, string> _dict;
+        public LoanCalcInputFromDict(Dictionary<string, string> dict)
+        {
+            _dict = dict;
+        }
+        public LoanCalcInput GetLoanCalcInput()
+        {
+            int parsedloanamount;
+            float parsedrate;
+            int parseddownpayment;
+            int parsedterm;
+            if (!int.TryParse(_dict["amount"], out parsedloanamount))
+            {
+                Console.WriteLine(string.Format("loan amount given {0} is not in the correct format", _dict["amount"]));
+            }
+            if (!float.TryParse(_dict["interest"], out parsedrate))
+            {
+                Console.WriteLine(string.Format("interest rate given {0} is not in the correct format", _dict["interest"]));
+            }
+            if (!int.TryParse(_dict["downpayment"], out parseddownpayment))
+            {
+                Console.WriteLine(string.Format("down payment amount given {0} is not in the correct format", _dict["downpayment"]));
+            }
+            if (!int.TryParse(_dict["term"], out parsedterm))
+            {
+                Console.WriteLine(string.Format("loan term given {0} is not in the correct format", _dict["term"]));
+            }
+
+            LoanCalcInput loanCalcInput = new LoanCalcInput()
+            {
+                amount = parsedloanamount,
+                interest = parsedrate,
+                downpayment = parseddownpayment,
+                term = parsedterm
+            };
+
+            return loanCalcInput;
+        }
+    }
+}
